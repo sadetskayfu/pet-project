caae3af9b4641547051d50fb44c40f47e2de6017
sadetskayfu <goblin_1444@mail.ru>
2025-04-27 03:40:34 +0300

    fix dialog height

--- src/shared/ui/Dialog/ui/DialogContent.tsx
+++ src/shared/ui/Dialog/ui/DialogContent.tsx
@@ -2,12 +2,12 @@ import {
 	FloatingFocusManager,
 	FloatingPortal,
 	useTransitionStatus,
-} from '@floating-ui/react'
-import { useDialogContext } from '../model/useDialogContext'
-import { ReactNode, useEffect, useRef } from 'react'
-import { classNames, Mods } from '@/shared/helpers/classNames'
-import { Overlay } from '@/shared/ui/Overlay'
-import styles from './style.module.scss'
+} from "@floating-ui/react"
+import { useDialogContext } from "../model/useDialogContext"
+import { ReactNode, useEffect, useRef } from "react"
+import { classNames, Mods } from "@/shared/helpers/classNames"
+import { Overlay } from "@/shared/ui/Overlay"
+import styles from "./style.module.scss"
 
 interface DialogContentProps {
 	id?: string
@@ -19,7 +19,14 @@ interface DialogContentProps {
 }
 
 export const DialogContent = (props: DialogContentProps) => {
-	const { id, className, containerClassName, children, style, zIndex = 1500 } = props
+	const {
+		id,
+		className,
+		containerClassName,
+		children,
+		style,
+		zIndex = 1500,
+	} = props
 
 	const {
 		context,
@@ -40,14 +47,14 @@ export const DialogContent = (props: DialogContentProps) => {
 		duration: 200,
 	})
 
-	if(referenceRef) {
+	if (referenceRef) {
 		refs.setReference(referenceRef.current)
 	}
 
 	useEffect(() => {
-		if(status === 'close') {
-			if(typeof returnFocus === 'boolean') {
-				if(returnFocus) {
+		if (status === "close") {
+			if (typeof returnFocus === "boolean") {
+				if (returnFocus) {
 					const referenceEl = refs.reference.current as HTMLElement | null
 					referenceEl?.focus()
 				}
@@ -58,24 +65,33 @@ export const DialogContent = (props: DialogContentProps) => {
 	}, [status, returnFocus, refs.reference])
 
 	const mods: Mods = {
-		[styles['open']]: status === 'open',
-		[styles['close']]: status === 'close',
+		[styles["open"]]: status === "open",
+		[styles["close"]]: status === "close",
 	}
 
 	if (!isMounted) return null
 
 	return (
 		<FloatingPortal>
-			<Overlay lockScroll zIndex={zIndex} open={status === 'open'} close={status === 'close'}>
+			<Overlay
+				lockScroll
+				zIndex={zIndex}
+				open={status === "open"}
+				close={status === "close"}
+			>
 				<FloatingFocusManager
 					initialFocus={initialFocusValue}
 					returnFocus={false}
 					context={context}
 					modal={true}
 				>
-					<div className={classNames(styles['dialog'], [containerClassName])} ref={refs.setFloating} role="presentation">
+					<div
+						className={classNames(styles["dialog"], [containerClassName])}
+						ref={refs.setFloating}
+						role="presentation"
+					>
 						<div
-							className={classNames(styles['content'], [className], mods)}
+							className={classNames(styles["content"], [className], mods)}
 							style={style}
 							ref={dialogRef}
 							aria-labelledby={labelId}
